a3dc6f37305fc2603861213245b6e9c96d27edb7
greenpickles03 <71419350+greenpickles03@users.noreply.github.com>
2023-02-08 11:34:53 +0800

    first commit

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "../service/employee";
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit{
+
+  employees!: Employee[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
