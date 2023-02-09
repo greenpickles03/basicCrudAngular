ad9ff8084a3fd3f936f78ea22e6639c67783432d
greenpickles03 <71419350+greenpickles03@users.noreply.github.com>
2023-02-09 14:33:53 +0800

    add modal component

---
+++ src/app/service/modal.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+
+  // @ts-ignore
+  private display: BehaviorSubject<'open' | 'close'> = new BehaviorSubject('close');
+
+  constructor() { }
+
+  watch() : Observable<'open' | 'close'> {
+    return this.display.asObservable();
+  }
+
+  open(){
+    this.display.next('open')
+  }
+
+  close(){
+    this.display.next('close');
+  }
+
+}
+
+
+
+
