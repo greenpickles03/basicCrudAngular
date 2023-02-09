4649b9423da6becdc1e565c8754efb462d879f53
greenpickles03 <71419350+greenpickles03@users.noreply.github.com>
2023-02-09 14:27:04 +0800

    add modal service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import {FormsModule} from "@angular/forms";
-import { EmployeeModalComponent } from './service/employee-modal/employee-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    CreateEmployeeComponent,
-    EmployeeModalComponent
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
